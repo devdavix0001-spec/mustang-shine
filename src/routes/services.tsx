85819cd10fc0f1295536aadb8d3d4f231cbd54f5
devdavix0001-spec <devdavix0001@gmail.com>
2026-09-10 17:41:17 +0100

    Update services.tsx

--- src/routes/services.tsx
+++ src/routes/services.tsx
@@ -12,7 +12,7 @@ import project8 from "@/assets/AtticEncapsulation-1.jpg";
 import project9 from "@/assets/cpm-1.jpg";
 import project10 from "@/assets/mbw-1.jpg";
 
-import project11 from "@/assets/old-site/mustang-project-11.jpg";
+import project11 from "@/assets/sp-1.jpg";
 import project12 from "@/assets/old-site/mustang-project-12.jpg";
 import project13 from "@/assets/old-site/mustang-project-13.jpg";
 
