aea5098e6242a612a8a5dc487b794f0fa3a69502
devdavix0001-spec <devdavix0001@gmail.com>
2026-09-15 17:45:43 +0100

    Update services.tsx

--- src/routes/services.tsx
+++ src/routes/services.tsx
@@ -12,7 +12,7 @@ import project8 from "@/assets/attic-encapsulation.jpg";
 import project9 from "@/assets/cpm-1.jpg";
 import project10 from "@/assets/mbw-1.jpg";
 
-import project11 from "@/assets/sp-1.jpg";
+import project11 from "@/assets/sound-proofing.jpeg";
 import project12 from "@/assets/re-as-1.jpg";
 import project13 from "@/assets/Ifpc-2.jpg";
 
