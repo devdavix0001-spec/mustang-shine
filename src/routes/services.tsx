0e9518410d03e1bbe9850c1a0d1e1afb5c2337ca
devdavix0001-spec <devdavix0001@gmail.com>
2026-09-15 17:26:48 +0100

    Update services.tsx

--- src/routes/services.tsx
+++ src/routes/services.tsx
@@ -5,7 +5,7 @@ import servicesImage from "@/assets/old-site/mustang-project-17.jpg";
 import project2 from "@/assets/Blown-In Attic Insulation.jpg";
 import project3 from "@/assets/Fiberglass Batt Insulation.jpg";
 import project4 from "@/assets/Spray Foam Insulation.jpg";
-import project5 from "@/assets/Insulation Removal.jpg";
+import project5 from "@/assets/insulation-removal-main.jpg";
 import project6 from "@/assets/atttic-1.jpg";
 import project7 from "@/assets/RadiantBarriers-1.jpg";
 import project8 from "@/assets/AtticEncapsulation-1.jpg";
